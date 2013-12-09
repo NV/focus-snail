1173fbd837f6a28a3936c9e8ad3b4e3682b3aa2c
Nikita Vasilyev <me@elv1s.ru>
2013-12-08 23:59:56 -0500

    Keep the built version in the repo so it can be accessed at http://nv.github.io/focus-snail/standalone/focus-snail.js

---
+++ standalone/focus-snail.js
@@ -0,0 +1,423 @@
+var focusSnail = (function() {
+	'use strict';
+
+var OFFSET_PX = 0;
+var MIN_WIDTH = 12;
+var MIN_HEIGHT = 8;
+
+var START_FRACTION = 0.4;
+var MIDDLE_FRACTION = 0.8;
+
+var focusSnail = {
+	enabled: true,
+	trigger: trigger
+};
+
+
+/**
+ * @param {Element} prevFocused
+ * @param {Element} target
+ */
+function trigger(prevFocused, target) {
+	if (svg) {
+		onEnd();
+	} else {
+		initialize();
+	}
+
+	var prev = dimensionsOf(prevFocused);
+	var current = dimensionsOf(target);
+
+	var left = 0;
+	var prevLeft = 0;
+	var top = 0;
+	var prevTop = 0;
+
+	var distance = dist(prev.left, prev.top, current.left, current.top);
+	var duration = animationDuration(distance);
+
+	function setup() {
+		var scroll = scrollOffset();
+		svg.style.left = scroll.left + 'px';
+		svg.style.top = scroll.top + 'px';
+		svg.setAttribute('width', win.innerWidth);
+		svg.setAttribute('height', win.innerHeight);
+		svg.classList.add('focus-snail_visible');
+		left = current.left - scroll.left;
+		prevLeft = prev.left - scroll.left;
+		top = current.top - scroll.top;
+		prevTop = prev.top - scroll.top;
+	}
+
+	var isFirstCall = true;
+
+	animate(function(fraction) {
+		if (isFirstCall) {
+			setup();
+			setGradientAngle(gradient, prevLeft, prevTop, prev.width, prev.height, left, top, current.width, current.height);
+			var list = getPointsList({
+				top: prevTop,
+				right: prevLeft + prev.width,
+				bottom: prevTop + prev.height,
+				left: prevLeft
+			}, {
+				top: top,
+				right: left + current.width,
+				bottom: top + current.height,
+				left: left
+			});
+			enclose(list, polygon);
+		}
+
+		var startOffset = fraction > START_FRACTION ? easeOutQuad((fraction - START_FRACTION) / (1 - START_FRACTION)) : 0;
+		var middleOffset = fraction < MIDDLE_FRACTION ? easeOutQuad(fraction / MIDDLE_FRACTION) : 1;
+		start.setAttribute('offset', startOffset * 100 + '%');
+		middle.setAttribute('offset', middleOffset * 100 + '%');
+
+		if (fraction >= 1) {
+			onEnd();
+		}
+
+		isFirstCall = false;
+	}, duration);
+}
+
+
+function animationDuration(distance) {
+	return Math.pow(constrain(distance, 32, 1024), 1/3) * 50;
+}
+
+
+function easeOutQuad(x) {
+	return 2*x - x*x;
+}
+
+
+var win = window;
+var doc = document;
+var docElement = doc.documentElement;
+var body = doc.body;
+
+var prevFocused = null;
+var animationId = 0;
+var keyDownTime = 0;
+
+
+docElement.addEventListener('keydown', function(event) {
+	if (!focusSnail.enabled) {
+		return;
+	}
+	var code = event.which;
+	// Show animation only upon Tab or Arrow keys press.
+	if (code === 9 || (code > 36 && code < 41)) {
+		keyDownTime = Date.now();
+	}
+}, false);
+
+
+docElement.addEventListener('blur', function(e) {
+	if (!focusSnail.enabled) {
+		return;
+	}
+	onEnd();
+	if (isJustPressed()) {
+		prevFocused = e.target;
+	} else {
+		prevFocused = null;
+	}
+}, true);
+
+
+docElement.addEventListener('focus', function(event) {
+	if (!prevFocused) {
+		return;
+	}
+	if (!isJustPressed()) {
+		return;
+	}
+	trigger(prevFocused, event.target);
+}, true);
+
+
+function setGradientAngle(gradient, ax, ay, aWidth, aHeight, bx, by, bWidth, bHeight) {
+	var centroidA = rectCentroid(ax, ay, aWidth, aHeight);
+	var centroidB = rectCentroid(bx, by, bWidth, bHeight);
+	var angle = Math.atan2(centroidA.y - centroidB.y, centroidA.x - centroidB.x);
+	var line = angleToLine(angle);
+	gradient.setAttribute('x1', line.x1);
+	gradient.setAttribute('y1', line.y1);
+	gradient.setAttribute('x2', line.x2);
+	gradient.setAttribute('y2', line.y2);
+}
+
+
+function rectCentroid(x, y, width, height) {
+	return {
+		x: x + width / 2,
+		y: y + height / 2
+	};
+}
+
+
+function angleToLine(angle) {
+	var segment = Math.floor(angle / Math.PI * 2) + 2;
+	var diagonal = Math.PI/4 + Math.PI/2 * segment;
+
+	var od = Math.sqrt(2);
+	var op = Math.cos(Math.abs(diagonal - angle)) * od;
+	var x = op * Math.cos(angle);
+	var y = op * Math.sin(angle);
+
+	return {
+		x1: x < 0 ? 1 : 0,
+		y1: y < 0 ? 1 : 0,
+		x2: x >= 0 ? x : x + 1,
+		y2: y >= 0 ? y : y + 1
+	};
+}
+
+
+/** @type {SVGSVGElement} */
+var svg = null;
+
+/** @type {SVGPolygonElement} */
+var polygon = null;
+
+/** @type SVGStopElement */
+var start = null;
+/** @type SVGStopElement */
+var middle = null;
+/** @type SVGStopElement */
+var end = null;
+
+/** @type SVGLinearGradientElement */
+var gradient = null;
+
+
+
+function htmlFragment() {
+	var div = doc.createElement('div');
+	div.innerHTML = '<svg id="focus-snail_svg" width="1000" height="800">\
+		<linearGradient id="focus-snail_gradient">\
+			<stop id="focus-snail_start" offset="0%" stop-color="rgb(91, 157, 217)" stop-opacity="0"/>\
+			<stop id="focus-snail_middle" offset="80%" stop-color="rgb(91, 157, 217)" stop-opacity="0.8"/>\
+			<stop id="focus-snail_end" offset="100%" stop-color="rgb(91, 157, 217)" stop-opacity="0"/>\
+		</linearGradient>\
+		<polygon id="focus-snail_polygon" fill="url(#focus-snail_gradient)"/>\
+	</svg>';
+	return div;
+}
+
+
+function initialize() {
+	var html = htmlFragment();
+	svg = getId(html, 'svg');
+	polygon = getId(html, 'polygon');
+	start = getId(html, 'start');
+	middle = getId(html, 'middle');
+	end = getId(html, 'end');
+	gradient = getId(html, 'gradient');
+	body.appendChild(svg);
+}
+
+
+function getId(elem, name) {
+	return elem.querySelector('#focus-snail_' + name);
+}
+
+
+function onEnd() {
+	if (animationId) {
+		cancelAnimationFrame(animationId);
+		animationId = 0;
+		svg.classList.remove('focus-snail_visible');
+	}
+}
+
+
+function isJustPressed() {
+	return Date.now() - keyDownTime < 42
+}
+
+
+function animate(onStep, duration) {
+	var start = Date.now();
+	(function loop() {
+		animationId = requestAnimationFrame(function() {
+			var diff = Date.now() - start;
+			var fraction = diff / duration;
+			onStep(fraction);
+			if (diff < duration) {
+				loop();
+			}
+		});
+	})();
+}
+
+
+function getPointsList(a, b) {
+	var x = 0;
+
+	if (a.top < b.top)
+		x = 1;
+
+	if (a.right > b.right)
+		x += 2;
+
+	if (a.bottom > b.bottom)
+		x += 4;
+
+	if (a.left < b.left)
+		x += 8;
+
+	var dict = [
+		[],
+		[0, 1],
+		[1, 2],
+		[0, 1, 2],
+		[2, 3],
+		[0, 1], // FIXME: do two polygons
+		[1, 2, 3],
+		[0, 1, 2, 3],
+		[3, 0],
+		[3, 0, 1],
+		[3, 0], // FIXME: do two polygons
+		[3, 0, 1, 2],
+		[2, 3, 0],
+		[2, 3, 0, 1],
+		[1, 2, 3, 0],
+		[0, 1, 2, 3, 0]
+	];
+
+	var points = rectPoints(a).concat(rectPoints(b));
+	var list = [];
+	var indexes = dict[x];
+	for (var i = 0; i < indexes.length; i++) {
+		list.push(points[indexes[i]]);
+	}
+	while (i--) {
+		list.push(points[indexes[i] + 4]);
+	}
+	return list;
+}
+
+
+function enclose(list, polygon) {
+	polygon.points.clear();
+	for (var i = 0; i < list.length; i++) {
+		var p = list[i];
+		addPoint(polygon, p);
+	}
+}
+
+
+function addPoint(polygon, point) {
+	var pt = polygon.ownerSVGElement.createSVGPoint();
+	pt.x = point.x;
+	pt.y = point.y;
+	polygon.points.appendItem(pt);
+}
+
+
+function rectPoints(rect) {
+	return [
+		{
+			x: rect.left,
+			y: rect.top
+		},
+		{
+			x: rect.right,
+			y: rect.top
+		},
+		{
+			x: rect.right,
+			y: rect.bottom
+		},
+		{
+			x: rect.left,
+			y: rect.bottom
+		}
+	];
+}
+
+
+function dimensionsOf(element) {
+	var offset = offsetOf(element);
+	return {
+		left: offset.left - OFFSET_PX,
+		top: offset.top - OFFSET_PX,
+		width: Math.max(MIN_WIDTH, element.offsetWidth) + 2*OFFSET_PX,
+		height: Math.max(MIN_HEIGHT, element.offsetHeight) + 2*OFFSET_PX
+	};
+}
+
+function offsetOf(elem) {
+	var rect = elem.getBoundingClientRect();
+	var scroll = scrollOffset();
+
+	var clientTop  = docElement.clientTop  || body.clientTop,
+	clientLeft = docElement.clientLeft || body.clientLeft,
+	top  = rect.top  + scroll.top  - clientTop,
+	left = rect.left + scroll.left - clientLeft;
+
+	return {
+		top: top || 0,
+		left: left || 0
+	};
+}
+
+function scrollOffset() {
+	var top = win.pageYOffset || docElement.scrollTop;
+	var left = win.pageXOffset || docElement.scrollLeft;
+	return {
+		top: top || 0,
+		left: left || 0
+	};
+}
+
+
+function dist(x1, y1, x2, y2) {
+	var dx = x1 - x2;
+	var dy = y1 - y2;
+	return Math.sqrt(dx*dx + dy*dy);
+}
+
+
+function constrain(amt, low, high) {
+	if (amt <= low) {
+		return low;
+	}
+	if (amt >= high) {
+		return high;
+	}
+	return amt;
+}
+
+
+	var style = doc.createElement('style');
+	style.textContent = "#focus-snail_svg {\
+	position: absolute;\
+	top: 0;\
+	right: 0;\
+	bottom: 0;\
+	left: 0;\
+	margin: 0;\
+	background: transparent;\
+	visibility: hidden;\
+	pointer-events: none;\
+	-webkit-transform: translateZ(0);\
+}\
+\
+#focus-snail_svg.focus-snail_visible {\
+	visibility: visible;\
+	z-index: 999;\
+}\
+\
+#focus-snail_polygon {\
+	stroke-width: 0;\
+}\
+";
+	body.appendChild(style);
+
+	return focusSnail;
+})();
